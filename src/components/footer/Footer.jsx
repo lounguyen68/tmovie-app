842fb5922f7bcf4f1b3187e2553fbeb202cccdc5
lounguyen68 <lou9.works@gmail.com>
2023-02-19 17:02:20 +0700

    Catalog Pages, search feature

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
+import './footer.scss';
+
+import { Link } from "react-router-dom";
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png'
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return ( 
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                        <Link to="/">tMovies</Link>
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                    <div className="footer__content__menu">
+                        <Link to="/">Home</Link>
+                        <Link to="/">Contact</Link>
+                        <Link to="/">Term of Service</Link>
+                        <Link to="/">About Us</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/">Live</Link>
+                        <Link to="/">FAN</Link>
+                        <Link to="/">Premium</Link>
+                        <Link to="/">Privacy Policy</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/">You must watch</Link>
+                        <Link to="/">Recent release</Link>
+                        <Link to="/">Top IMDB</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Footer;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+
+import PageHeader from '../components/page-header/PageHeader';
+import MovieGrid from "../components/movie-grid/MovieGrid";
+
+import { category as cate } from '../api/tmdbApi';
+
 const Catalog = () => {
-    return <div>Catalog</div>;
+
+    const { category } = useParams();
+
+    // console.log(category);
+
+
+    return (
+        <div>
+            <PageHeader>{category === cate.movie ? 'Movies' : 'TV Series'}</PageHeader>
+            <div className="container">
+                <div className="section mb-3">
+                    <MovieGrid category={category}/>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Catalog;
